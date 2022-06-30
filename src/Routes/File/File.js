218c357f2a5730fabaa6ca9e974ba93dd7a3ff0c
agonzalezf <adriangonzalezfilgueira@gmail.com>
2022-06-30 20:57:17 +0200

    Styling is my passion so I devoted myself to the css, mui and react all knowing

--- src/Routes/File/File.js
+++ src/Routes/File/File.js
@@ -1,4 +1,4 @@
-import { IconButton, Fab, Breadcrumbs, Link } from '@mui/material';
+import { Card, Container } from '@mui/material';
 import { Add, Delete, Download } from '@mui/icons-material';
 import { useToken } from '../../TokenContext';
 import { Navigate } from 'react-router-dom';
@@ -8,7 +8,39 @@ import { useEffect, useState } from 'react';
 function File() {
     return (
         <div className="File">
-            
+            <Container maxWidth="sm"> 
+                <Card
+                    variant="outlined"
+                    sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                    gap: "5vh",
+                    padding: "2vh",
+                    background: "rgba(255, 255, 255, 0.25)",
+                    borderColor: "white",
+                    boxSizing: "border-box",
+                    borderRadius: "3vh",
+                    py: 17,
+                    
+                    }}
+                    >
+                <img
+                    style={{
+                    objectFit: "contain",
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "3px 3px 1px 1px",
+                    }}
+                    src={``}
+                    alt={``}
+                    loading="lazy"
+                />
+                <p>File Name:</p>
+                <p>Upload Date:</p>
+                <p>Size:</p>
+                </Card>
+            </Container>
         </div>
     );
 }
