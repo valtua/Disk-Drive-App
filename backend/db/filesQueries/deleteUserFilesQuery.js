afb27fd3e0e8f219c115e2c4adac70602c42067a
agonzalezf <adriangonzalezfilgueira@gmail.com>
2022-07-03 10:34:08 +0200

    Format

--- backend/db/filesQueries/deleteUserFilesQuery.js
+++ backend/db/filesQueries/deleteUserFilesQuery.js
@@ -1,22 +1,22 @@
-const getConnection = require('../getConnection');
+const getConnection = require("../getConnection");
 
 // Función con query para eliminar un archivo
 const deleteFilesQuery = async (idUser, id) => {
-    let connection;
+  let connection;
 
-    try {
-        // Conectamos a la base de datos
-        connection = await getConnection();
+  try {
+    // Conectamos a la base de datos
+    connection = await getConnection();
 
-        // Realizamos la query
-        await connection.query(
-            `DELETE FROM files WHERE idUser = ? AND id = ?`,
-            [idUser, id]
-        );
-    } finally {
-        // Liberamos la conexión
-        if (connection) connection.release();
-    }
+    // Realizamos la query
+    await connection.query(`DELETE FROM files WHERE idUser = ? AND id = ?`, [
+      idUser,
+      id,
+    ]);
+  } finally {
+    // Liberamos la conexión
+    if (connection) connection.release();
+  }
 };
 
 module.exports = deleteFilesQuery;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 // Tecnologia para hacer disponibles las variables de entorno
-require('dotenv').config();
+require("dotenv").config();
 
 // ---------------------- IMPORTS ----------------------
 
 // Requerimos las tecnologías necesarias
-const cors = require('cors');
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const morgan = require('morgan');
+const cors = require("cors");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
 
 // Requerimos la autentificación del usuario
-const authUser = require('./middleware/authUser');
+const authUser = require("./middleware/authUser");
 
 // Almacenamos en esta variable los datos que contiene el archivo .env (base de datos)
 const { BACKEND_PORT } = process.env;
@@ -19,23 +19,23 @@ const { BACKEND_PORT } = process.env;
 
 // Requerimos los controladores para los archivos, carpetas y usuarios
 const {
-    deleteFile,
-    downloadFile,
-    newFile,
-    newFileInFolder,
-} = require('./controllers/files/filesExports');
+  deleteFile,
+  downloadFile,
+  newFile,
+  newFileInFolder,
+} = require("./controllers/files/filesExports");
 const {
-    deleteFolder,
-    downloadFolder,
-    newFolder,
-} = require('./controllers/folders/foldersExports');
+  deleteFolder,
+  downloadFolder,
+  newFolder,
+} = require("./controllers/folders/foldersExports");
 const {
-    newUser,
-    loginUser,
-    modifyUser,
-    getUserSpace,
-    getOwnUser,
-} = require('./controllers/users/usersExports');
+  newUser,
+  loginUser,
+  modifyUser,
+  getUserSpace,
+  getOwnUser,
+} = require("./controllers/users/usersExports");
 
 // Declaramos una variable que contiene la tecnología express, para el manejo de endpoints
 const app = express();
@@ -45,67 +45,67 @@ app.use(morgan(`dev`));
 app.use(express.json());
 app.use(fileUpload());
 app.use(cors());
-app.use(express.static(__dirname + '/uploads/photos'));
+app.use(express.static(__dirname + "/uploads/photos"));
 
 // ---------------------- ENDPOINTS ----------------------
 
 // Registrar un nuevo usuario
-app.post('/register', newUser);
+app.post("/register", newUser);
 
 // Login
-app.post('/login', loginUser);
+app.post("/login", loginUser);
 
 // Modificar un usuario
-app.put('/user', authUser, modifyUser);
+app.put("/user", authUser, modifyUser);
 
 // Ver archivos y carpetas
-app.get('/disk', authUser, getUserSpace);
+app.get("/disk", authUser, getUserSpace);
 
 // Ver información del usuario logueado
-app.get('/user', authUser, getOwnUser);
+app.get("/user", authUser, getOwnUser);
 
 // Subir un nuevo archivo
-app.post('/upload', authUser, newFile);
+app.post("/upload", authUser, newFile);
 
 // Subir un archivo dentro de una carpeta
-app.post('/upload/:folderId', authUser, newFileInFolder);
+app.post("/upload/:folderId", authUser, newFileInFolder);
 
 // Descargar un archivo
-app.get('/download/file/:fileId', authUser, downloadFile);
+app.get("/download/file/:fileId", authUser, downloadFile);
 
 // Borrar un archivo
-app.delete('/file/:fileId', authUser, deleteFile);
+app.delete("/file/:fileId", authUser, deleteFile);
 
 // Crear una nueva carpeta
-app.post('/folder', authUser, newFolder);
+app.post("/folder", authUser, newFolder);
 
 // Descargar una carpeta
-app.get('/download/folder/:folderId', authUser, downloadFolder);
+app.get("/download/folder/:folderId", authUser, downloadFolder);
 
 // Borrar una carpeta
-app.delete('/folder/:folderId', authUser, deleteFolder);
+app.delete("/folder/:folderId", authUser, deleteFolder);
 
 // --------------------------------------------------------
 
 // MIDDLEWARE ERROR
 
 app.use((err, req, res, next) => {
-    console.error(err);
+  console.error(err);
 
-    res.status(err.statusCode || 500).send({
-        status: 'error',
-        message: err.message,
-    });
+  res.status(err.statusCode || 500).send({
+    status: "error",
+    message: err.message,
+  });
 });
 
 app.use((req, res) => {
-    res.status(404).send({
-        status: 'error',
-        message: 'Not found',
-    });
+  res.status(404).send({
+    status: "error",
+    message: "Not found",
+  });
 });
 
 // Iniciamos el servidor
 app.listen(BACKEND_PORT, () => {
-    console.log(`Server listening at http://localhost:${BACKEND_PORT}`);
+  console.log(`Server listening at http://localhost:${BACKEND_PORT}`);
 });
