ada74f6a162962149664f965d7324e1da47d31e8
davidgomezfrieiro <davidgomezfrieiro@gmail.com>
2022-07-04 10:45:14 +0200

    token context comments

--- src/TokenContext.js
+++ src/TokenContext.js
@@ -1,27 +1,33 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from 'react';
 
+// Creación del contexto
 const TokenContext = createContext(null);
 
+// Creación del componente para proveer los datos del contexto
 export const TokenProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem("token"));
+    // Estado que almacenará el valor del token
+    const [token, setToken] = useState(localStorage.getItem('token'));
 
-  const setTokenInLocalStorage = (newToken) => {
-    if (!newToken) {
-      localStorage.removeItem("token");
-    } else {
-      localStorage.setItem("token", newToken);
-    }
+    // Guardamos el token en el local storage
+    const setTokenInLocalStorage = (newToken) => {
+        if (!newToken) {
+            localStorage.removeItem('token');
+        } else {
+            localStorage.setItem('token', newToken);
+        }
 
-    setToken(newToken);
-  };
+        // Añadimos valor al estado de token con el código recibido
+        setToken(newToken);
+    };
 
-  return (
-    <TokenContext.Provider value={[token, setTokenInLocalStorage]}>
-      {children}
-    </TokenContext.Provider>
-  );
+    return (
+        <TokenContext.Provider value={[token, setTokenInLocalStorage]}>
+            {children}
+        </TokenContext.Provider>
+    );
 };
 
+// Creación del hook para utilizar el contexto
 export const useToken = () => {
-  return useContext(TokenContext);
+    return useContext(TokenContext);
 };
