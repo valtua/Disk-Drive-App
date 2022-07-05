5ce9d35df36c1b5f7d38cbf179e6d79feca3972c
davidgomezfrieiro <davidgomezfrieiro@gmail.com>
2022-07-05 13:21:07 +0200

    css modifications, disk fixes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,11 @@
-import "./Footer.css";
+import './Footer.css';
 
 function Footer() {
-  return (
-    <footer>
-      <h5>(C) Disk Drive 2022</h5>
-    </footer>
-  );
+    return (
+        <footer>
+            <h5>(C) Disk Drive 2022</h5>
+        </footer>
+    );
 }
 
 export default Footer;
