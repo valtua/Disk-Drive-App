4743574b4e0d5bfa92abec5944bd2d95a161aba2
davidgomezfrieiro <davidgomezfrieiro@gmail.com>
2022-07-05 12:16:50 +0200

    rerouting not defined endpoints

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
-import Home from "./Routes/Home/Home";
-import Disk from "./Routes/Disk/Disk";
-import Profile from "./Routes/Profile/Profile";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Routes/Home/Home';
+import Disk from './Routes/Disk/Disk';
+import Profile from './Routes/Profile/Profile';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/disk" element={<Disk />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="" element={<Home />} />
+                <Route path="/disk" element={<Disk />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="*" element={<Home />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
