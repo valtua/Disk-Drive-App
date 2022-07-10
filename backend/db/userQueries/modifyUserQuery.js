c5ad76a3df81e6d95d98f93692cf6fb13a68c148
davidgomezfrieiro <davidgomezfrieiro@gmail.com>
2022-07-10 12:20:12 +0200

    required inputs label feedback, placeholders color, modify profile values, modify email bug fix

--- backend/db/userQueries/modifyUserQuery.js
+++ backend/db/userQueries/modifyUserQuery.js
@@ -1,30 +1,44 @@
-const { generateError } = require("../../helpers");
-const getConnection = require("../getConnection");
+const { generateError } = require('../../helpers');
+const getConnection = require('../getConnection');
 
 // Función con query para modificar un usuario
 const modifyUserQuery = async (name, email, biography, photo, id) => {
-  let connection;
+    let connection;
 
-  try {
-    // Conectamos a la base de datos
-    connection = await getConnection();
+    try {
+        // Conectamos a la base de datos
+        connection = await getConnection();
 
-    // Realizamos la query, modificar usuario
-    const user = await connection.query(
-      `UPDATE users SET 
+        // Realizamos la query
+        const [users] = await connection.query(
+            `SELECT id FROM users WHERE email = ?`,
+            [email]
+        );
+
+        // Lanzamos un error cuando la longitud de 'users' es mayor que 0
+        if (users.length > 0) {
+            throw generateError(
+                'Ya existe un usuario asociado a ese email',
+                409
+            );
+        }
+
+        // Realizamos la query, modificar usuario
+        const user = await connection.query(
+            `UPDATE users SET 
             name = COALESCE(?, name), 
             email = COALESCE(?, email), 
             biography = COALESCE(?, biography), 
             photo = COALESCE(?, photo) WHERE id = ?; `,
-      [name, email, biography, photo, id]
-    );
+            [name, email, biography, photo, id]
+        );
 
-    // Retornamos el usuario
-    return user;
-  } finally {
-    // Liberamos la conexión
-    if (connection) connection.release();
-  }
+        // Retornamos el usuario
+        return user;
+    } finally {
+        // Liberamos la conexión
+        if (connection) connection.release();
+    }
 };
 
 module.exports = modifyUserQuery;
